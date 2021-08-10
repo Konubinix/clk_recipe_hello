d572122df282abbc2c5c98946f1b265179bac720
Samuel Loury <konubinixweb@gmail.com>
2021-08-10 09:16:58 +0200

    Use clk instead of click-project

--- python/say.py
+++ python/say.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import click
 
-from click_project.decorators import (
+from clk.decorators import (
     argument,
     flag,
     option,
@@ -14,23 +14,25 @@ from click_project.decorators import (
     table_format,
     table_fields,
 )
-from click_project.lib import (
+from clk.lib import (
     TablePrinter,
     call,
 )
-from click_project.config import config
-from click_project.log import get_logger
-from click_project.types import DynamicChoice
-
+from clk.config import config
+from clk.log import get_logger
+from clk.types import DynamicChoice
 
 LOGGER = get_logger(__name__)
 
-
 import cowsay
+
 
 @command()
 @argument("what", help="What to say")
-@option("--who", type=click.Choice(cowsay.char_names), help="Who says", default="cow")
+@option("--who",
+        type=click.Choice(cowsay.char_names),
+        help="Who says",
+        default="cow")
 def say(what, who):
     "Say something with style"
     getattr(cowsay, who)(what)
